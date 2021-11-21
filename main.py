3987052d924ff698bb41813e77eb39cf9cbeeb6f
dv-opel <d.verich1990@gmail.com>
2021-11-21 12:08:40 +0200

    create window game

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 500, 500
+FPS = 60
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.display.update()
+    clock.tick(FPS)
